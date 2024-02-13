172d77cc7d87ba7d9d2a417186ec1be004477f6c
npntraining <npn.npntraining@gmail.com>
2024-02-13 13:54:52 +0530

    Add code

--- start-multi-node-cluster.bat
+++ start-multi-node-cluster.bat
@@ -1,12 +1,27 @@
-docker container kill master
-docker container kill slave1
-docker container kill slave2
-docker container kill slave3
-docker container remove master
-docker container remove slave1
-docker container remove slave2
-docker container remove slave3
+@echo off
+setlocal enabledelayedexpansion
+
+:menu
+echo Which containers do you want to kill and remove?
+echo 1. Login to existing cluster
+echo 2. Delete and add containers
+echo 3. None
+set /p choice=Enter your choice (1-2): 
+
+if "%choice%"=="1" (
+    goto compose_up
+) else if "%choice%"=="2" (
+    set containers=master slave1 slave2 slave3
+) else (
+    echo Invalid choice. Please try again.
+    goto menu
+)
+
+for %%c in (%containers%) do (
+    docker container kill %%c
+    docker container rm %%c
+)
+
+:compose_up
 docker-compose -f multi-node-cluster/docker-compose.yml up -d 
 docker exec -it master bash
-docker exec -it slave1 bash
-docker exec -it slave2 bash
